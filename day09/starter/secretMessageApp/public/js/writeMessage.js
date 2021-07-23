734322f8754437f2cb3d6101255cc87df5920889
aarush3002 <aarush3002@gmail.com>
2021-07-23 03:19:37 +0000

    day 9 independent work

--- day09/starter/secretMessageApp/public/js/writeMessage.js
+++ day09/starter/secretMessageApp/public/js/writeMessage.js
@@ -1,9 +1,51 @@
+function contains(password, allowedChars) {
+ 
+    for (i = 0; i < password.length; i++) {
+ 
+            var char = password.charAt(i);
+ 
+             if (allowedChars.indexOf(char) >= 0) { return true; }
+ 
+         }
+ 
+     return false;
+}
+
 const submitMessage = () => {
     console.log("Submitting message...");
     const passcodeInput = document.querySelector("#passcode");
     const messageInput = document.querySelector("#message");
     const passcodeValue = passcodeInput.value;
     const messageValue = messageInput.value;
+
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ 
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+ 
+    var digits = "0123456789";
+ 
+    var splChars ="!@#$%&*()";
+ 
+    var ucaseFlag = contains(passcodeValue, uppercase);
+ 
+    var lcaseFlag = contains(passcodeValue, lowercase);
+ 
+    var digitsFlag = contains(passcodeValue, digits);
+ 
+    var splCharsFlag = contains(passcodeValue, splChars);
+ 
+    if(passcodeValue.length>=8 && ucaseFlag && lcaseFlag && digitsFlag && splCharsFlag)
+        console.log("good password");
+    else {
+        alert("Your password is not strong enough. Please include lower and uppercase letters, numbers, and special characters.")
+        passcodeInput.value = "";
+        return 0;
+    }
+
+    if (messageValue.length >= 90) {
+        console.log("hi");
+        alert("Your message is approaching the maximum character limit.");
+    }
     
     // send data to firebase
     firebase.database().ref().push({
