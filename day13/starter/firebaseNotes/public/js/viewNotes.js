0da5c3cb3b911ae13a783d2a48d2f83da65e5dad
aarush3002 <aarush3002@gmail.com>
2021-07-28 22:03:23 +0000

    day 13 work

--- day13/starter/firebaseNotes/public/js/viewNotes.js
+++ day13/starter/firebaseNotes/public/js/viewNotes.js
@@ -1,9 +1,11 @@
+let googleUserId, editedNoteId; //global variables
+
 window.onload = (event) => {
   // Use this to retain user state between html pages.
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log('Logged in as: ' + user.displayName);
-      const googleUserId = user.uid;
+      googleUserId = user.uid;
       getNotes(googleUserId);
     } else {
       // If not logged in, navigate back to login page.
@@ -21,31 +23,116 @@ const getNotes = (userId) => {
 };
 
 const renderDataAsHtml = (data) => {
-  let cards = ``;
-  for(const noteItem in data) {
-    const note = data[noteItem];
-    // For each note create an HTML card
-    cards += createCard(note)
-  };
+    
+    let dict = {};
+    for(const noteItem in data) {
+        //console.log(data);
+        dict[noteItem] = data[noteItem]["title"];
+    }
+    
+    // Create items array
+    var items = Object.keys(dict).map(function(key) {
+        return [key, dict[key]];
+    });
+    
+    // Sort the array based on the second element
+    items.sort(function(first, second) {
+        if (second[1].localeCompare(first[1])) {
+            return second[1].localeCompare(first[1]);
+        } else if (first[1].localeCompare(second[1])){
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    
+    let cards = ``;
+    for(const index in items) {
+        const noteItem = items[index][0];
+        const note = data[noteItem];
+        // For each note create an HTML card
+        cards += createCard(note, noteItem);
+    };
   // Inject our string of HTML into our viewNotes.html page
   document.querySelector('#app').innerHTML = cards;
 };
 
-const createCard = (note) => {
-  let innerHTML = "";
-  innerHTML += `<div class="column is-one-quarter">`
-  innerHTML += `<div class="card">`
-  innerHTML += `<header class="card-header">`
-  innerHTML += `<p class="card-header-title">`
-  innerHTML += `${note.title}`
-  innerHTML += `</p>`
-  innerHTML += `</header>`
-  innerHTML += `<div class="card-content">`
-  innerHTML += `<div class="content">`
-  innerHTML += `${note.text}`
-  innerHTML += `</div>`
-  innerHTML += `</div>`
-  innerHTML += `</div>`
-  innerHTML += `</div>`
-  return innerHTML;
+const createCard = (note, noteId) => {
+    return `
+        <div class="column is-one-quarter">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">${note.title}</p>
+                </header>
+                <div class="card-content">
+                    <div class="content">${note.text}</div>
+                </div>
+                <footer class="card-footer">
+                    <a id="${noteId}" href="#" class="card-footer-item"
+                        onclick="deleteNote('${noteId}')">
+                        Delete
+                    </a>
+                    <a id="${noteId}" href="#" class="card-footer-item"
+                        onclick="editNote('${noteId}')">
+                        Edit
+                    </a>
+                </footer>
+            </div>
+        </div>`;
 };
+
+// href="#" is a link to the current page, so it'll reload the page and show the updates.
+
+const deleteNote = (noteId) => {
+    if (confirm("Are you sure you want to delete this note?")) {
+        console.log(`Deleting note: ${noteId}`);
+        firebase.database().ref(`users/${googleUserId}/${noteId}`).remove();
+    }
+};
+
+const editNote = (noteId) => {
+    console.log(`Editing note: ${noteId}`);
+
+    editedNoteId = noteId;
+    // show the modal dialogue
+    const editNoteModal = document.querySelector("#editNoteModal");
+    editNoteModal.classList.toggle("is-active");
+    // Get the text from the note in the database
+    const notesRef = firebase.database().ref(`users/${googleUserId}/${noteId}`);
+    notesRef.on('value', snapshot => {
+        const data = snapshot.val();
+        console.log(data);
+
+
+        // show the text from the database in the modal
+
+        // set the text into an editable form (input)
+        document.querySelector("#editTitleInput").value = data["title"];
+        document.querySelector("#editTextInput").value = data["text"];        
+    });
+
+    // save updated text to the database.
+
+    // Hide the modal box ... once the user has made their selection
+}
+
+const closeEditModal = () => {
+    const editNoteModal = document.querySelector("#editNoteModal");
+    editNoteModal.classList.toggle("is-active");
+}
+
+const saveEditedNote = () => {
+    const newTitle = document.querySelector("#editTitleInput").value;
+    const newText = document.querySelector("#editTextInput").value;
+
+    firebase.database().ref(`users/${googleUserId}/${editedNoteId}`)
+        .set({
+            title: newTitle,
+            text: newText
+        });
+    closeEditModal();
+}
+
+const archiveNote = (noteId) => {
+    //add functionality.
+}
